130f32e11674daae2fb55a779e1de8357fe5f9fe
Ratiu Marius-Andrei <mariusratiu@yahoo.com>
2020-07-29 13:58:50 +0300

    Created a default project for the app

---
+++ app/front_end/app/src/main/java/com/example/ott_officetimetracker/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.ott_officetimetracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
