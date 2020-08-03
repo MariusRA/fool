0d094490fe8eef11ae6925551f08e2d78ff25748
Ratiu Marius-Andrei <mariusratiu@yahoo.com>
2020-08-03 16:41:58 +0300

    Created the layout for register and login

---
+++ app/front_end/OTTOfficeTimeTracker/app/src/main/java/com/example/officetimetracker/LoginActivity.java
@@ -0,0 +1,24 @@
+package com.example.officetimetracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        //Intent intent =getIntent();
+    }
+
+    public void onClickNeedReg(View view){
+
+        Intent intent =new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+}
