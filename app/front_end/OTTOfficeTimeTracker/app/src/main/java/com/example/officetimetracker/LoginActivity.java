da7e13fdcbaa480ac052228c6532e635c07faf63
Ratiu Marius-Andrei <mariusratiu@yahoo.com>
2020-08-06 18:06:59 +0300

    Functionalities for login and register (tested and working)

--- app/front_end/OTTOfficeTimeTracker/app/src/main/java/com/example/officetimetracker/LoginActivity.java
+++ app/front_end/OTTOfficeTimeTracker/app/src/main/java/com/example/officetimetracker/LoginActivity.java
@@ -3,15 +3,49 @@ package com.example.officetimetracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText username, pass;
+    Button login;
+
+    SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        username=findViewById(R.id.nameLog);
+        pass=findViewById(R.id.passLog);
+        login=findViewById(R.id.btnLog);
+
+        preferences=getSharedPreferences("UserInfo",0);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String usernameValue=username.getText().toString();
+                String passwordValue=pass.getText().toString();
+
+                String registeredUsername=preferences.getString("Username","");
+                String registeredPassword=preferences.getString("Password","");
+
+                if(usernameValue.equals(registeredUsername) && passwordValue.equals(registeredPassword)){
+                    Intent intent=new Intent(LoginActivity.this,MainActivity.class);
+                    startActivity(intent);
+                }
+                else{
+                    Toast.makeText(LoginActivity.this,"Invalid Username or Password",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
         //Intent intent =getIntent();
     }
--- app/front_end/OTTOfficeTimeTracker/app/src/main/java/com/example/officetimetracker/RegisterActivity.java
+++ app/front_end/OTTOfficeTimeTracker/app/src/main/java/com/example/officetimetracker/RegisterActivity.java
@@ -3,15 +3,53 @@ package com.example.officetimetracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    EditText email, username,pass;
+    Button register;
+
+    SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        email=findViewById(R.id.emailReg);
+        username=findViewById(R.id.nameReg);
+        pass=findViewById(R.id.passReg);
+        register=findViewById(R.id.btnSubmitReg);
+
+        preferences=getSharedPreferences("UserInfo",0);
+
+        register.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                String emailValue=email.getText().toString();
+                String userValue=username.getText().toString();
+                String passValue=pass.getText().toString();
+
+                if(userValue.length()>1 && emailValue.length()>1 && passValue.length()>1) {
+                    SharedPreferences.Editor editor = preferences.edit();
+                    editor.putString("Email", emailValue);
+                    editor.putString("Username", userValue);
+                    editor.putString("Password", passValue);
+                    editor.apply();
+                    Toast.makeText(RegisterActivity.this,"User Registered!",Toast.LENGTH_SHORT).show();
+                    Intent intent=new Intent(RegisterActivity.this,LoginActivity.class);
+                    startActivity(intent);
+                }
+                else{
+                    Toast.makeText(RegisterActivity.this,"Fill all fields for registration",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
         //Intent intent =getIntent();
     }
@@ -21,4 +59,5 @@ public class RegisterActivity extends AppCompatActivity {
         Intent intent=new Intent(this,LoginActivity.class);
         startActivity(intent);
     }
+
 }
