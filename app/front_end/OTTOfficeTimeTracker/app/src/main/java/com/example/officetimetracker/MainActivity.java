c8d99fab8417638877f6a0e375a04a983d0bbb5c
Ratiu Marius-Andrei <mariusratiu@yahoo.com>
2020-08-19 18:25:16 +0300

    Added menu layout and created the other needed activities(empty)

--- app/front_end/OTTOfficeTimeTracker/app/src/main/java/com/example/officetimetracker/MainActivity.java
+++ app/front_end/OTTOfficeTimeTracker/app/src/main/java/com/example/officetimetracker/MainActivity.java
@@ -2,13 +2,65 @@ package com.example.officetimetracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button profile,projects,reports,signOut,newSession;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        profile=findViewById(R.id.btnProfile);
+        projects=findViewById(R.id.btnProjects);
+        reports=findViewById(R.id.btnReports);
+        signOut=findViewById(R.id.btnSignOut);
+        newSession=findViewById(R.id.btnNewSession);
+
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        projects.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),ProjectsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        reports.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),ReportsActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        newSession.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),NewSessionActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        signOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
