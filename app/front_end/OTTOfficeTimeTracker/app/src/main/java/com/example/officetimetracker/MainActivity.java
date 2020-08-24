6b19284f73bc03c58d921da6160b77805aa35936
Ratiu Marius-Andrei <mariusratiu@yahoo.com>
2020-08-24 16:47:59 +0300

    User profile layout

--- app/front_end/OTTOfficeTimeTracker/app/src/main/java/com/example/officetimetracker/MainActivity.java
+++ app/front_end/OTTOfficeTimeTracker/app/src/main/java/com/example/officetimetracker/MainActivity.java
@@ -9,22 +9,21 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button profile,projects,reports,signOut,newSession;
+    Button profile, projects, reports, newSession;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        profile=findViewById(R.id.btnProfile);
-        projects=findViewById(R.id.btnProjects);
-        reports=findViewById(R.id.btnReports);
-        signOut=findViewById(R.id.btnSignOut);
-        newSession=findViewById(R.id.btnNewSession);
+        profile = findViewById(R.id.btnProfile);
+        projects = findViewById(R.id.btnProjects);
+        reports = findViewById(R.id.btnReports);
+        newSession = findViewById(R.id.btnNewSession);
 
         profile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(getApplicationContext(),ProfileActivity.class);
+                Intent intent = new Intent(getApplicationContext(), ProfileActivity.class);
                 startActivity(intent);
             }
         });
@@ -32,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         projects.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(getApplicationContext(),ProjectsActivity.class);
+                Intent intent = new Intent(getApplicationContext(), ProjectsActivity.class);
                 startActivity(intent);
             }
         });
@@ -40,7 +39,7 @@ public class MainActivity extends AppCompatActivity {
         reports.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(getApplicationContext(),ReportsActivity.class);
+                Intent intent = new Intent(getApplicationContext(), ReportsActivity.class);
                 startActivity(intent);
             }
         });
@@ -48,18 +47,11 @@ public class MainActivity extends AppCompatActivity {
         newSession.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(getApplicationContext(),NewSessionActivity.class);
+                Intent intent = new Intent(getApplicationContext(), NewSessionActivity.class);
                 startActivity(intent);
             }
         });
 
-        signOut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(getApplicationContext(),LoginActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 
 
