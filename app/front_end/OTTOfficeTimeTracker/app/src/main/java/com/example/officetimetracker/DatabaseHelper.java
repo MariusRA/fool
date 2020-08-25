093d18846038fcc0a575a25543b9464eb248293a
Ratiu Marius-Andrei <mariusratiu@yahoo.com>
2020-08-25 20:08:19 +0300

    User profile re-worked + Project activity

--- app/front_end/OTTOfficeTimeTracker/app/src/main/java/com/example/officetimetracker/DatabaseHelper.java
+++ app/front_end/OTTOfficeTimeTracker/app/src/main/java/com/example/officetimetracker/DatabaseHelper.java
@@ -19,11 +19,13 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("create table users(id integer primary key autoincrement,username text,email text,password text)");
+        db.execSQL("create table projects(id integer primary key autoincrement,managerId integer,name text,description text)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("drop table if exists users");
+        db.execSQL("drop table if exists projects");
     }
 
     public boolean insert(String username, String email, String password) {
@@ -37,6 +39,32 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             return false;
         }
         return true;
+    }
+
+    public boolean insertProject(String name, String description) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("name", name);
+        contentValues.put("description", description);
+        long result = db.insert("projects", null, contentValues);
+        if (result == -1) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean update(String newUsername, String newEmail, String newPassword, String oldPassword) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.rawQuery("Select * from users where username=? and password=?", new String[]{oldPassword});
+        if (cursor.getCount() == 1) {
+            ContentValues contentValues = new ContentValues();
+            contentValues.put("username", newUsername);
+            contentValues.put("email", newEmail);
+            contentValues.put("password", newPassword);
+            db.update("users", contentValues, "password=?", new String[]{oldPassword});
+            return true;
+        }
+        return false;
     }
 
     public boolean checkEmail(String email) {
@@ -56,4 +84,22 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         }
         return false;
     }
+
+    public boolean loadProfile(User user) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery("Select * from users where username=? and password=?", new String[]{user.getUsername(), user.getPassword()});
+        if (cursor.getCount() < 1) {
+            return false;
+        }
+        cursor.moveToFirst();
+        user.setEmail(cursor.getString(cursor.getColumnIndex("email")));
+        return true;
+    }
+
+    public Cursor dataView() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery("Select id,name,description from projects",null);
+        return cursor;
+    }
+
 }
---
+++ app/front_end/OTTOfficeTimeTracker/app/src/main/java/com/example/officetimetracker/User.java
@@ -0,0 +1,57 @@
+package com.example.officetimetracker;
+
+public class User {
+
+    private static User userInstance = null;
+
+    private String username;
+    private String email;
+    private String password;
+    private String id;
+
+
+    private User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public static User getInstance(String username, String password) {
+
+        if (userInstance == null) {
+            userInstance = new User(username, password);
+        }
+        return userInstance;
+    }
+
+    public static void releaseInstance() {
+        userInstance = null;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
