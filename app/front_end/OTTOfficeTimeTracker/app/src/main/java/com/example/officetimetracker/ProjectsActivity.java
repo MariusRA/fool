905eedd3fb5f44954698b823580f86428cf49630
Ratiu Marius-Andrei <mariusratiu@yahoo.com>
2020-09-08 19:40:54 +0300

    Layout for the projects activity

--- app/front_end/OTTOfficeTimeTracker/app/src/main/java/com/example/officetimetracker/ProjectsActivity.java
+++ app/front_end/OTTOfficeTimeTracker/app/src/main/java/com/example/officetimetracker/ProjectsActivity.java
@@ -4,58 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.database.Cursor;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+
 
 public class ProjectsActivity extends AppCompatActivity {
 
     DatabaseHelper mydbh;
-    TextView projectsList;
-
-    EditText pName, pDescription;
-    Button addProject;
+    TableLayout table;
+    TableRow row;
+    Button addProject,deleteProject,editProject,seeProjectInfo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_projects);
 
-      /*  pName = findViewById(R.id.projectName);
-        pDescription = findViewById(R.id.projectDescription);
-        addProject = findViewById(R.id.btnAddProject);
-
-        mydbh = new DatabaseHelper(getApplicationContext());
-
-        StringBuilder stringBuilder = new StringBuilder();
-
-        Cursor cursor = mydbh.dataView();
-
-        if (cursor.getCount() < 1) {
-            stringBuilder.append("No data ");
-        } else {
-            stringBuilder.append("");
-            while (cursor.moveToNext()) {
-                stringBuilder.append("\nProject id: " + cursor.getInt(cursor.getColumnIndex("id")) +
-                        "\nProject name: " + cursor.getString(cursor.getColumnIndex("name")) +
-                        "\nProject description: " + cursor.getString(cursor.getColumnIndex("description"))
-                        + "\n");
-            }
-        }
+        table=findViewById(R.id.tlProjects);
+        addProject=findViewById(R.id.btnAddProject);
+        deleteProject=findViewById(R.id.btnDeleteProject);
+        editProject=findViewById(R.id.btnEditProject);
+        seeProjectInfo=findViewById(R.id.btnSeeInfo);
 
 
-        addProject.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                boolean result = mydbh.insertProject(pName.getText().toString(), pDescription.getText().toString());
-                if (result) {
-                    Toast.makeText(getApplicationContext(), "New project added!", Toast.LENGTH_LONG).show();
-                } else {
-                    Toast.makeText(getApplicationContext(), "Problem adding the new project!", Toast.LENGTH_LONG).show();
-                }
-            }
-        });*/
+
     }
 }
